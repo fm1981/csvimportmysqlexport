fa065aae4b4fe5e56f59f668b131928d11f54d32
fm1981 <48291838+fm1981@users.noreply.github.com>
2019-03-08 10:36:19 +0000

    Update script_two.py

--- script_two.py
+++ script_two.py
@@ -6,13 +6,13 @@ import collections
 import warnings
 import mysql.connector
 
-mydb = mysql.connector.connect(user='***', passwd='***', database='***', host='85.10.205.173', port = '3306')
+mydb = mysql.connector.connect(user='fmorgan', passwd='U#qUC76d', database='quantix_test', host='85.10.205.173', port = '3306')
 print("\nConnection to DB established\n")
 mycursor = mydb.cursor()
-mycursor.execute("SHOW columns FROM animal")
+mycursor.execute("SHOW columns FROM table1")
 print([column[0] for column in mycursor.fetchall()])
 
-animal_average = mycursor.execute("SELECT AVG(height) AS average FROM animal")
+animal_average = mycursor.execute("SELECT AVG(temp) AS average FROM table1")
 
 result = mycursor.fetchall()
 
