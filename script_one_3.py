a26be10f842ade1b3a41ecffe751ea1bf4a8a747
fm1981 <48291838+fm1981@users.noreply.github.com>
2019-03-08 15:23:53 +0000

    Update script_one_3.py

--- script_one_3.py
+++ script_one_3.py
@@ -36,39 +36,38 @@ def find_type(s):
     else:
         return 'varchar(255)'
 
-def find_common(l, default='varchar(255)'):
-    # Return most common from list
-
-    if l:
-        for dt_type in ('text', 'bigint'):
-            if dt_type in l:
-                return dt_type
+def find_common(l):
         return max(l, key=l.count)
-    return default
+
 
 def find_col_types(input_file, max_rows=1000):
     #Find the type for each CSV column
 
     csv_types = collections.defaultdict(list)
     reader = csv.reader(open(input_file))
+    #i = 0
 
     # test the first few rows for their data types
-    for row_i, row in enumerate(reader):
-        if row_i == 0:
-            header = row
+    for row_counter, row_contents in enumerate(reader):
+        if row_counter == 0:
+            header = row_contents
+            print(header)
         else:
-            for col_i, s in enumerate(row):
+            for col_counter, s in enumerate(row_contents):
                 data_type = find_type(s)
-                csv_types[header[col_i]].append(data_type)
+                csv_types[header[col_counter]].append(data_type)
+                #print(csv_types)
+                #i = i+1
+                #print(i)
 
-        if row_i == max_rows:
+        if row_counter == max_rows:
             break
 
     # take the most common data type for each row
     return [find_common(csv_types[col]) for col in header]
 
 def gen_schema(table, header, col_types):
-    #Generate the schema for this table from given types and columns
+    #Generate the schema for this table
     schema_sql = """CREATE TABLE IF NOT EXISTS %s (
         id int NOT NULL AUTO_INCREMENT,""" % table
 
@@ -87,19 +86,19 @@ def gen_insert(table, header):
     return 'INSERT INTO %s (%s) VALUES (%s);' % \
         (table, field_names, field_markers)
 
-def format_header(row):
+#def format_header(row):
     #Format column names to remove illegal characters and duplicates
 
-    safe_col = lambda s: re.sub('\W+', '_', s.lower()).strip('_')
-    header = []
-    counts = collections.defaultdict(int)
-    for col in row:
-        col = safe_col(col)
-        counts[col] += 1
-        if counts[col] > 1:
-            col = '{}{}'.format(col, counts[col])
-        header.append(col)
-    return header
+#    safe_col = lambda s: re.sub('\W+', '_', s.lower()).strip('_')
+#    header = []
+#    counts = collections.defaultdict(int)
+#    for col in row:
+#        col = safe_col(col)
+#        counts[col] += 1
+#        if counts[col] > 1:
+#            col = '{}{}'.format(col, counts[col])
+#        header.append(col)
+#    return header
 
 
 mydb = mysql.connector.connect(user='fmorgan', passwd='U#qUC76d', database='quantix_test', host='85.10.205.173', port = '3306')
@@ -111,25 +110,26 @@ col_types = find_col_types(csvfile)
 print(col_types)
 table = 'table1'
 
-
 header = None
-for i, row in enumerate(csv.reader(open(csvfile))):
-    if header:
-        mycursor.execute(insert_sql, row)
-    else:
-        header = (row)
-        n = len(header)
-        for col in range(n):
-          header[col] = (header[col].encode('ascii', 'ignore')).decode("utf-8")
+with open(csvfile) as f:
+    reader = csv.reader(f)
+    for row in reader:
+        if header:
+            mycursor.execute(insert_sql, row)
+        else:
+            header = (row)
+            n = len(header)
+            for col in range(n):
+              header[col] = (header[col].encode('ascii', 'ignore')).decode("utf-8")
 
-        schema_sql = gen_schema(table, header, col_types)
-        print(schema_sql)
-
-        # create table
-        mycursor.execute(schema_sql)
-        print('Inserting rows ...')
-# SQL string for inserting data
-        insert_sql = gen_insert(table, header)
+            schema_sql = gen_schema(table, header, col_types)
+            print("This is the schema:\n")
+            print(schema_sql)
+            # create table
+            mycursor.execute(schema_sql)
+            print('Inserting rows ...')
+            # SQL string for inserting data
+            insert_sql = gen_insert(table, header)
 
 
 # commit rows to database
